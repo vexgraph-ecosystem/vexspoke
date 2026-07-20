f9d51bf76c5a40041157b1a1b0d46c8c5cebbb75
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-07-20 14:22:45 +0800

    draft(oop): add @Draft and @Intention annotations to Generic and Inheritance classes

--- src/oop/Generic.java
+++ src/oop/Generic.java
@@ -1,9 +1,14 @@
 package oop;
 
-public class Generic
-{
-    public Generic()
-    {
+import annotation.Draft;
+import annotation.Intention;
+import annotation.Volatile;
 
-    }
+@Draft
+@Volatile
+@Intention("Draft marker and type layout holder for off-heap generic parameters and type descriptor mappings")
+public class Generic {
+
+    public Generic() {}
 }
+
--- src/oop/Inheritance.java
+++ src/oop/Inheritance.java
@@ -1,7 +1,14 @@
-package oop;public class Inheritance 
-{
-    public Inheritance()
-    {
-    
-    }
+package oop;
+
+import annotation.Draft;
+import annotation.Intention;
+import annotation.Volatile;
+
+@Draft
+@Volatile
+@Intention("Draft marker and subtype tree inspector for off-heap class hierarchy and polymorphism dispatch")
+public class Inheritance {
+
+    public Inheritance() {}
 }
+
