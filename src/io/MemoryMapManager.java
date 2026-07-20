a66e68c5a53f57598c053f2bba046f5bcf6f90c1
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-07-20 16:05:39 +0800

    refactor(io): rename MmapManager to MemoryMapManager to match layout standards

--- src/io/MemoryMapManager.java
+++ src/io/MemoryMapManager.java
@@ -10,12 +10,13 @@ import oop.TypeRegister;
  */
 @Draft
 @Intention("File I/O mapping wrapper using MemorySegment.mapFile to allow direct, zero-copy structural loads from disk to native RAM.")
-public final class MmapManager {
+public final class MemoryMapManager {
 
     @Required
-    public static final int CLASS_ID = TypeRegister.ID_MMAP_MANAGER;
+    public static final int CLASS_ID = TypeRegister.ID_MEMORY_MAP_MANAGER;
 
-    private MmapManager() {}
+    private MemoryMapManager() {}
+
 
     public static int classId() {
         return CLASS_ID;
