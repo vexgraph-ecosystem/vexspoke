9f87d2435befd0fb2031f35a54bc3fce2fba256d
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-07-21 19:54:28 +0800

    refactor: remove MemoryRegistry reference from Set.java

--- src/struct/Set.java
+++ src/struct/Set.java
@@ -6,7 +6,6 @@ import annotation.Intention;
 import annotation.Required;
 import lang.Class;
 import nio.ForeignMemory;
-import nio.MemoryRegistry;
 import oop.TypeRegister;
 import util.Hash;
 
@@ -39,7 +38,6 @@ public final class Set {
     static {
         poolArena = Arena.ofShared();
         active = true;
-        MemoryRegistry.register(Set::freeAll);
     }
 
     private Set() {}
