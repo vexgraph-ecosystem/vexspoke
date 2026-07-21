996073bdbab604111a198469b9f6a996012403af
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-07-21 19:54:28 +0800

    refactor: remove MemoryRegistry reference from Deque.java

--- src/struct/Deque.java
+++ src/struct/Deque.java
@@ -6,7 +6,6 @@ import annotation.Draft;
 import annotation.Intention;
 import annotation.Required;
 import nio.ForeignMemory;
-import nio.MemoryRegistry;
 import oop.Stride;
 import oop.TypeRegister;
 
@@ -34,7 +33,6 @@ public final class Deque {
     static {
         poolArena = Arena.ofShared();
         active = true;
-        MemoryRegistry.register(Deque::freeAll);
     }
 
     private Deque() {}
