01f74a7fd77282bd9f1defa31daf67e2475b6166
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-07-27 17:54:14 +0800

    update KeyEvent.java

--- src/event/KeyEvent.java
+++ src/event/KeyEvent.java
@@ -1,5 +1,7 @@
 package event;
 
+import input.KeyResolve;
+
 /**
  * High-level OOP listener interface for hardware key events.
  * 
@@ -7,7 +9,7 @@ package event;
  * guaranteeing zero Thread 0 blocking or GC pressure from event objects.
  */
 public interface KeyEvent {
-    void onKeyDown(int key);
-    void onKeyUp(int key);
-    void onKeyRepeat(int key);
+    void onKeyDown(KeyResolve resolve);
+    void onKeyUp(KeyResolve resolve);
+    void onKeyRepeat(KeyResolve resolve);
 }
