1ee78f22ad0179179c75e03b5ffcca66f1837710
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-07-29 22:43:33 +0800

    feat(cli): draft CommandParser tokenization logic

---
+++ src/cli/CommandParser.java
@@ -0,0 +1,14 @@
+package cli;
+
+import annotation.Draft;
+
+@Draft
+public class CommandParser {
+
+    @Draft
+    public static long parse(long rawStringPointer) {
+        // TODO: Tokenize the raw primitive.string pointer by splitting on spaces
+        // TODO: Return an off-heap struct pointer representing the parsed Command
+        return 0L;
+    }
+}
