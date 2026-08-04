90449f61d3200328964240faa01abdb5c1415a5c
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-08-04 16:30:43 +0800

    fix(objects): update Global

--- src/objects/Global.java
+++ src/objects/Global.java
@@ -1,10 +1,18 @@
 package objects;
 
+import annotation.Intention;
+
 // global variable, this is where the variable is being associated with a global context...
 public class Global
 {
-    public Global()
-    {
 
-    }
+    // [purpose]
+    // the purpose of the global variable is that the gloabel variable will be used to make
+    // a pointer that is dedicated for a single variable (whether it be a struct, a primitive,
+    // an array of objects, etc. it can act as just a pointer, it doesnt allocate a array for
+    // each thread. shall be allocate as global when allocated for games.
+
+
+    @Intention("[purpose] line [n]")
+    private Global() {}
 }
