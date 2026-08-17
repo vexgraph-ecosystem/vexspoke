bb2daca8b84ce92ca1dd66865fab4d15943e7612
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-08-17 15:24:19 +0800

    refactor(objects): delegate Future free and struct to Object helpers

--- src/objects/Future.java
+++ src/objects/Future.java
@@ -44,16 +44,12 @@ public class Future
 
     public static void free(long ptr)
     {
-        if (ptr == 0L) return;
-        long struct = ForeignMemory.getLong(ptr);
-        if (struct != 0L) ForeignMemory.freeNative(struct);
-        Bit64.free(ptr);
+        Object.free(ptr);
     }
 
     private static long struct(long ptr)
     {
-        if (ptr == 0L) throw new NullPointerException("Accessing NULL off-heap pointer!");
-        return ForeignMemory.getLong(ptr);
+        return Object.struct(ptr);
     }
 
     public static boolean isGiven(long ptr)
