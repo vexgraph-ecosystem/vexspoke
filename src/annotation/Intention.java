d939f1024577c424b648b8d1d0a47a865c1d2b22
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-08-18 18:33:08 +0800

    refactor(annotation): change Intention retention policy to SOURCE

--- src/annotation/Intention.java
+++ src/annotation/Intention.java
@@ -4,7 +4,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 // when the annotation is being intended, must be annotated on top of header/class for a reason @annot(params)
-@Retention(RetentionPolicy.RUNTIME)
+// Source-only retention: stripped completely by javac at compile time (0 bytes in bytecode, 0 image heap footprint)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Intention {
     String value() default "";
 }
