91335e3154fb557f7785567713773608e3e32ff7
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-08-18 18:35:37 +0800

    refactor(annotation): change Required retention policy to SOURCE

--- src/annotation/Required.java
+++ src/annotation/Required.java
@@ -4,6 +4,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 // need to be put as required when the parts are needed to be put on every class (e.g. every class should be part of one method, etc.)
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Required
 {}
