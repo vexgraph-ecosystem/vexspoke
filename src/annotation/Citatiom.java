c54a1d7ed45d2289f4071c78a1c2e18e3a6c33e5
vexgraph-dev <305705943+vexgraph-dev@users.noreply.github.com>
2026-08-18 18:35:37 +0800

    refactor(annotation): change Citatiom retention policy to SOURCE

--- src/annotation/Citatiom.java
+++ src/annotation/Citatiom.java
@@ -1,7 +1,11 @@
 package annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 // we are citing things to the CITATION.md
 // the IEEE way as well...
+@Retention(RetentionPolicy.SOURCE)
 public @interface Citatiom
 {
     int cite();
